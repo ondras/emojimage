1db077b829d90fc45471ecccbc581fae91134bf1
Ondřej Žára <ondrej.zara@gmail.com>
2023-02-10 17:41:53 +0100

    ts

---
+++ emojimage.ts
@@ -0,0 +1,12 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+
+const headers = {"content-type":"image/svg+xml"};
+
+async function onRequest(r: Request) {
+	let path = new URL(r.url).pathname.substring(1);
+	const svg = `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0,0,100,100'><text y='.9em' font-size='90'>${path}</text></svg>`;
+	return new Response(svg, {headers})
+}
+
+serve(onRequest);
