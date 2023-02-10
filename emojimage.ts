5f240d9232a4021fb7304e1cb0e5e908f2e54bc3
Ondřej Žára <ondrej.zara@gmail.com>
2023-02-10 17:44:27 +0100

    decode

--- emojimage.ts
+++ emojimage.ts
@@ -5,6 +5,7 @@ const headers = {"content-type":"image/svg+xml"};
 
 async function onRequest(r: Request) {
 	let path = new URL(r.url).pathname.substring(1);
+	path = decodeURIComponent(path);
 	const svg = `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0,0,100,100'><text y='.9em' font-size='90'>${path}</text></svg>`;
 	return new Response(svg, {headers})
 }
