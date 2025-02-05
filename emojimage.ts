16c8f841b5baed3dd0f1c6e8d9982eb2e1a332ed
Ondřej Žára <ondrej.zara@gmail.com>
2025-02-05 21:37:34 +0100

    Update emojimage.ts

--- emojimage.ts
+++ emojimage.ts
@@ -8,7 +8,7 @@ async function onRequest(r: Request) {
 	let path = url.pathname.substring(1);
 	path = decodeURIComponent(path);
 	let style = url.searchParams.get("style") || "";
-	const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,100,100"><text y=".9em" font-size="90" style="${style}">${path}</text></svg>`;
+	const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,100,100"><text x="50" y=".9em" text-anchor="middle" font-size="90" style="${style}">${path}</text></svg>`;
 	return new Response(svg, {headers})
 }
 
